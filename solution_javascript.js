ea62717907f616aa68b72c1afe0e8e3ad58e00b4
Nikunj Prajapati <nikunjd.np@gmail.com>
2020-07-10 14:45:18 -0400

    Co-op fall challange 2020 - changes.

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,44 @@
 class EventSourcer {
+
   constructor() {
     this.value = 0;
+    this.eventTrack = [0];
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+  add(num) {
+    this.value += num;
+    this.eventTrack.push(this.value);
+  }
+
+  subtract(num) {
+    this.value -= num;
+    this.eventTrack.push(this.value)
+  }
+
+  undo() {
+    if (this.eventTrack.length > 1) {
+      this.value = this.eventTrack[this.eventTrack.indexOf(this.value) - 1];
+    }
+
+  }
+
+  redo() {
+    if (this.eventTrack.length > 1) {
+      this.callRedoTime += 1;
+    }
+    this.callRedoTime += this.callRedoTime;
+    this.value = this.eventTrack[this.eventTrack.indexOf(this.value) + this.callRedoTime];
+  }
+
+  bulk_undo(num) {
+    this.value = this.eventTrack[this.eventTrack.lastIndexOf(this.value) - num];
+  }
+
+  bulk_redo(num) {
+    if (this.eventTrack.length > 1) {
+      this.value = this.eventTrack[this.eventTrack.indexOf(this.value) + num];
+    }
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
